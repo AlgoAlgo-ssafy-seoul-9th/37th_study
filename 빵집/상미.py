059ca951780b38b2da72a5714a37629dfbdd9a47
sangmihwang <01071184366a@gmail.com>
2024-06-04 12:33:34 +0900

    상미: 빵집

--- 빵집/상미.py
+++ 빵집/상미.py
@@ -2,9 +2,9 @@ import sys
 input = sys.stdin.readline
 
 def dfs(x, y):
-    # 갈 수 없는 경우: 벗어났거나, 이미 방문한 곳이거나, 건물이면 False
-    if x < 0 or x >= R or y < 0 or y >= C or visited[x][y] or arr[x][y] == 'x':
-        return False
+    # # 갈 수 없는 경우: 벗어났거나, 이미 방문한 곳이거나, 건물이면 False
+    # if x < 0 or x >= R or y < 0 or y >= C or visited[x][y] or arr[x][y] == 'x':
+    #     return False
     
     # 마지막 열에 도달하면 True
     if y == C - 1:
@@ -15,14 +15,13 @@ def dfs(x, y):
     
     # 오른쪽 위, 오른쪽, 오른쪽 아래로 이동
     for dir in [-1, 0, 1]:
-        if dfs(x + dir, y + 1):
-            return True
-    
+        if 0 <= x + dir < R and 0 <= y+1 < C and not visited[x+dir][y+1] and arr[x+dir][y+1] != 'x':
+            if dfs(x + dir, y + 1):
+                return True
     return False
 
 R, C = map(int, input().split())
 arr = [input().strip() for _ in range(R)]
-# print(arr)
 visited = [[0] * C for _ in range(R)]
 
 cnt = 0
